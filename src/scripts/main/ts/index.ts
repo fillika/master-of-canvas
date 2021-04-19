8dfdfe3de95d9ffe03bcc8157c49c6e67f1b5344
Aleksandr M <alfie.mir@gmail.com>
2021-04-19 13:26:10 +0300

    Добавил функции отрисовки изображения. Добавил функцию, которая делает изображение серым

--- src/scripts/main/ts/index.ts
+++ src/scripts/main/ts/index.ts
@@ -1,6 +1,2 @@
 // import './react/index'
-// import './modules/module_one';
-
-console.log("Let's start");
-
-document.querySelector('main')?.insertAdjacentText('beforeend', 'Canvas');
+import './modules/canvas1';
