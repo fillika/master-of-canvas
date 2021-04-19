dcca3bab1970af8460d329b9f6d67a869db28da8
Aleksandr M <alfie.mir@gmail.com>
2021-04-19 14:44:49 +0300

    Создали функцию, которая получает все пиксели и высчитывает яркость каждого из них

--- src/scripts/main/ts/modules/utils.ts
+++ src/scripts/main/ts/modules/utils.ts
@@ -4,14 +4,15 @@ function createImage(src: string, canvas: HTMLCanvasElement, ctx: CanvasRenderin
   const image1 = new Image();
   image1.src = src;
 
-  image1.addEventListener('load', () => {
-    let particlesArray = [];
-    const numberOfParticles = 5000;
+  const particlesArray = createParticlesArray(canvas, ctx, 5000);
 
-    // Создаем частицы
-    for (let k = 0; k < numberOfParticles; k++) {
-      particlesArray.push(new Particle(canvas, ctx));
-    }
+  image1.addEventListener('load', () => {
+    ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+    const mappedImage = createMappedImage(canvas, ctx);
+
+    console.log(mappedImage);
+
+    // -----
 
     animate(canvas, ctx, particlesArray, image1);
   });
@@ -24,6 +25,7 @@ function animate(
   image: HTMLImageElement
 ) {
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
   ctx.globalAlpha = 0.05;
   ctx.fillStyle = 'rgb(0,0,0)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -35,6 +37,53 @@ function animate(
   }
 
   requestAnimationFrame(() => animate(canvas, ctx, particlesArray, image));
+}
+
+function createParticlesArray(
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+  numberOfParticles: number
+): Particle[] {
+  const particlesArray = [];
+
+  // Создаем частицы
+  for (let k = 0; k < numberOfParticles; k++) {
+    particlesArray.push(new Particle(canvas, ctx));
+  }
+  return particlesArray;
+}
+
+function calcRelativeBrightness(r: number, g: number, b: number) {
+  return Math.sqrt(r * r * 0.299 + g * g * 0.587 + b * b * 0.114) / 100;
+}
+
+function createMappedImage(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D): number[][][] {
+  // Получем пиксели
+  const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const mappedImage = [];
+
+  /**
+   * Для начала Я прохожу по каждому ряду. (высота canvas)
+   * Все для того, чтобы получить массив элементов с яркостью (brightness) каждого пикселя
+   */
+  for (let y = 0; y < canvas.height; y++) {
+    let row = [];
+
+    for (let x = 0; x < canvas.width; x++) {
+      const red = pixels.data[y * 4 * pixels.width + x * 4];
+      const green = pixels.data[y * 4 * pixels.width + x * 4 + 1];
+      const blue = pixels.data[y * 4 * pixels.width + x * 4 + 2];
+      const brightness = calcRelativeBrightness(red, green, blue);
+
+      const cell = [brightness];
+
+      row.push(cell);
+    }
+
+    mappedImage.push(row);
+  }
+
+  return mappedImage;
 }
 
 /**
